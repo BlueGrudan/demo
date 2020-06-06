e62c618ff9bdb43c7a6cbfe900f175c4b03fe02b
BlueGrudan <670147604@qq.com>
2020-06-06 14:18:37 +0800

    fist commit

---
+++ src/main/java/com/example/demo2/Reverse.java
@@ -0,0 +1,10 @@
+package com.example.demo2;
+
+import java.util.WeakHashMap;
+
+public class Reverse {
+    public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("abcdefg");
+        System.out.println(str.reverse());
+    }
+}
